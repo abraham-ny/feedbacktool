6c9b32965ea1ea83d97022396b30d454f7e6f4b2
Abraham .M <abummoja3@gmail.com>
2025-07-06 18:39:45 +0300

    Add project files.

---
+++ FeedbackTooll/Devtool.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FeedbackTooll
+{
+    /// <summary>
+    /// Interaction logic for Devtool.xaml
+    /// </summary>
+    public partial class Devtool : Window
+    {
+        public Devtool()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //open browser and visit url
+
+        }
+    }
+}
